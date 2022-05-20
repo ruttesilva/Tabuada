58bb2f0a675909d23a833eee982cd85f97d850cc
Rutte Oliveira <rosrosrutte@gmail.com>
2022-05-20 20:41:31 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿double numero, contador, produto;
+
+Console.Write("Tabuada do número: ");
+numero = Convert.ToDouble(Console.ReadLine()!);
+
+if (numero < 0 || numero > 10)
+
+{
+    Console.WriteLine("Digite números de 0 a 10");
+}
+
+else
+{
+    contador = 0;
+
+    while (contador <= 10)
+    {
+        produto = numero * contador;
+        Console.WriteLine($"{numero} * {contador++} = {produto}");
+
+    }
+
+}
